7ff996efb2a2f6b70e810698984d4506ecfa09a8
PS49550_DiepHuynhNhuMy <mydiep025@gmail.com>
2025-11-10 14:55:16 +0700

    nop lab 1

---
+++ Lab1/Hello.c
@@ -0,0 +1,13 @@
+# include <stdio.h>// thu vien
+
+int main(){
+    //printf("Hello, World");// xuat ra man hinh
+    //prinf("\nHello World");
+    int a, b;
+    char ten[30];//cap phat ki tu
+    printf("Nhap a va b;");
+    scanf("%d%d", &a,&b);
+    printf("Nhap vao ten toi:");
+    scanf("%s", ten);
+    return 0;
+}
